b3d03920d72716ef21440ace8ca93b662ad265b2
daw2 <fatinebh23@gmail.com>
2023-12-18 14:02:41 +0100

    base de datos prueba

--- Transacciones/src/main/java/org/example/Main.java
+++ Transacciones/src/main/java/org/example/Main.java
@@ -4,17 +4,24 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-     String sentenciaSQL= "SELECT * FROM personas";
+     String sentenciaSQL= "SELECT * FROM actores";
 
-     try(Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/mi-base");
+
+     String usuario="postgres";
+     String contrasenya = "postgres";
+
+     try(Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/mi-base", usuario,contrasenya);
          PreparedStatement sentencia= con.prepareStatement(sentenciaSQL)){
 
          ResultSet resultados = sentencia.executeQuery();
 
          while (resultados.next()){
             String nombre= resultados.getString("nombre");
-            int edad= resultados.getInt("edad");
-
+            Integer edad= resultados.getInt("edad");
+             Integer id= resultados.getInt("id");
+             System.out.println(nombre);
+             System.out.println(edad);
+             System.out.println(id);
          }
 
      }catch (SQLException e){
